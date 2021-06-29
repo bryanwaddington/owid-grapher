74f023ac631c74dffa33bdcf8b2c1eab1dc94d30
Bryan Waddington <bryan@bryanwaddington.com>
2021-06-29 15:19:15 +0100

    logging DB_NAME

--- itsJustJavascript/settings/serverSettings.js
+++ itsJustJavascript/settings/serverSettings.js
@@ -36,6 +36,9 @@ dotenv_1.default.config({ path: `${baseDir}/.env` });
 const clientSettings = __importStar(require("./clientSettings"));
 const Util_1 = require("../clientUtils/Util");
 const serverSettings = (_a = process.env) !== null && _a !== void 0 ? _a : {};
+
+console.log('DB_NAME', serverSettings.DB_NAME);
+
 exports.BASE_DIR = baseDir;
 exports.ENV = clientSettings.ENV;
 exports.ADMIN_SERVER_PORT = clientSettings.ADMIN_SERVER_PORT;
